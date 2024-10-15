6ac6a1962850cba483e2b72745bf13b1e51b684d
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-16 01:36:50 +0200

    Push Sprint 1

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,16 +1,16 @@
 import classes from './css/Footer.module.css'; 
-import { mainMenus } from '../../constants/main-menus'
+import { mainMenus } from '../../data/main-menus'
 
 function Footer() {
     return (
         <footer className={classes.footer}>
                 <div className='container p-2'>
                     <div className='row mt-5 mb-5'> 
-                        <div className='col-md-3 col-lg-2 col-xl-2 mx-auto'>
-                            <h2 class="fw-bold mb-4 text-white">Blogr</h2>  
+                        <div className='col-12 col-md-auto xs-auto text-center text-md-left'>
+                            <h2 class="fw-bold mb-4 text-white">NSK Site</h2>  
                         </div>
                         {mainMenus.map((footerMenu) => (
-                            <div className="col-auto mx-auto" key={footerMenu.groupTitle}>
+                            <div className="col-12 col-md-auto mx-auto text-center text-md-left" key={footerMenu.groupTitle}>
                                 <h4 className='fw-bold mb-3 text-white'>
                                     {footerMenu.groupTitle}
                                 </h4>
