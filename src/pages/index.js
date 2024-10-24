c68de0ce9364c52ef46c331d69043e139c00f51b
Haniche Zahir <zahir.haniche@inetum.com>
2024-10-24 10:42:19 +0200

    Add NODE_ENV in index JS

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import MainHome from "../components/Home/MainHome";
 
+let logger;
+if (typeof window === 'undefined' && process.env.NODE_ENV === 'development') {
+  // Nous sommes dans un environnement côté serveur et en développement
+  logger = require('../config/winston');
+}
+
 export default function Home() {
   return (
     <>
@@ -15,3 +21,15 @@ export default function Home() {
   );
 }
 
+export async function getServerSideProps() {
+  // Vérifiez si le logger est défini (cela signifie que nous sommes côté serveur et en développement)
+  if (logger) {
+    logger.info("Page d'accueil rendue avec succès - Niveau Info");
+    logger.error("Une erreur fictive est survenue - Niveau Error");
+    logger.debug("Debugging les données du serveur - Niveau Debug");
+  }
+
+  return {
+    props: {}, // Possible de passer des props ici si nécessaire
+  };
+}
